bd288b591ed9634fcae1cf9fee69bf9bc434658d
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-08-31 16:48:10 +0545

    feat: app.js file added

---
+++ js/app.js
@@ -0,0 +1,11 @@
+import { UI } from "./ui.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  ui.renderNotes();
+
+  const addBtn = document.getElementById("addNoteBtn");
+  addBtn.addEventListener("click", () => {
+    ui.addNote();
+  });
+});
