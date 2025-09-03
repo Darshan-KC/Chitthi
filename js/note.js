d7c26fe27be1fe763a5f698be261afaab7a97a88
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-09-03 21:45:17 +0545

    feat: css and note js file update

--- js/note.js
+++ js/note.js
@@ -3,9 +3,25 @@ export class Note {
     this.id = id;
     this.content = content;
     this.createdAt = createdAt;
+    this.colorClass = this.getRandomColor();
+  }
+
+  getRandomColor() {
+    const colors = [
+      "note-color-1",
+      "note-color-2",
+      "note-color-3",
+      "note-color-4",
+      "note-color-5",
+    ];
+    return colors[Math.floor(Math.random() * colors.length)];
   }
 
   render() {
-    return `<div class="note">${this.content}</div>`;
+    return `
+      <div class="note ${this.colorClass}" data-id="${this.id}">
+        <p contenteditable="true">${this.content}</p>
+      </div>
+    `;
   }
 }
